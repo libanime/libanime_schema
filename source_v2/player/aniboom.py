009d940cd1742c084af7f0f40ef5fdef01652717
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-06-05 11:27:31 +0300

    players parsers schemas

---
+++ source_v2/player/aniboom.py
@@ -0,0 +1,84 @@
+from ssc_codegen import DictSchema, ItemSchema, ListSchema, D, N, R
+
+
+class AniboomResult(ItemSchema):
+    """Extract MPD and M3U8 urls
+
+    Required `referer="https://animego.org/` HEADER
+
+    USAGE:
+        1. GET <PLAYER_LINK> (e.g. https://aniboom.one/embed/6BmMbB7MxWO?episode=1&translation=30)
+        2. PARSE. If pre-unescape response before parse - css selector may not find attribute
+        3. For video playing, url required next headers:
+
+        - Referer="https://aniboom.one/"
+        - Accept-Language="ru-RU"  # INCREASE DOWNLOAD SPEED with this static value
+        - Origin="https://aniboom.one"
+    ISSUES:
+        - 403 Forbidden if request sent not from CIS region
+        - KEYS SHOULD BE STARTED IN Title case else hls/mpd links returns 403 error
+        - Sometimes, aniboom backend missing MPD key and returns M3U8 url. Check this value before usage:
+
+        https://github.com/vypivshiy/ani-cli-ru/issues/29
+
+        Expected json signature (LOOK at dash.src and hls.src keys):
+
+        { ...
+        "dash":"{\"src\":\"https:.../abcdef.mpd\",\
+        "type\":\"application\\\/dash+xml\"}",
+        "hls":"{\"src\":\"https:...\\\/master_device.m3u8\",
+        \"type\":\"application\\\/x-mpegURL\"}"
+
+        ... }
+
+        MAYBE returns this:
+
+         { ...
+        "dash":"{\"src\":\"https:...master_device.m3u8\",\
+        "type\":\"application\\\/dash+xml\"}",
+        "hls":"{\"src\":\"https:...master_device.m3u8\",
+        \"type\":\"application\\\/x-mpegURL\"}"
+
+        ... }
+
+
+
+    """
+
+    # can be json unmarshal
+    # eg signature:
+    # {"id":"WlmXYpNLMK0",
+    # "error":"/video-error/WlmXYpNLMK0",
+    # "domain":"animego.org",
+    # "cdn":"/cdn/WlmXYpNLMK0",
+    # "counter":"/counter/WlmXYpNLMK0",
+    # "duration":"1437",
+    # "poster":"https://i1.boom-img.com/wl/WlmXYpNLMK0/mqdefault.webp",
+    # "thumbnails":"https://i1.boom-img.com/wl/WlmXYpNLMK0/thumbnails/thumbnails.vtt",
+    #
+    # YES, THIS KEYS contains STRING
+    # "dash":"{
+    #   "src":"https://lily.yagami-light.com/wl/WlmXYpNLMK0/qqppjxsz6fvkf.mpd",
+    #   "type":"application/dash+xml"
+    #   }",
+    # "hls":"{
+    #   "src":"https://lily.yagami-light.com/wl/WlmXYpNLMK0/master_device.m3u8",
+    #   "type":"application/x-mpegURL"}",
+    #
+    # "quality":true, "qualityVideo":1080, "vast":true, "country":"RU",
+    # "platform":"Linux","rating":"16+","nshowbl":false,"limitRate":false,
+    # "aBlocklimitRate":false}
+    data_parameters = (D().css('#video').attr('data-parameters')
+                       .replace('\\', '').replace('&quot;', '"')  # json unescape
+                       )
+    hls = (D().css('#video').attr('data-parameters')
+            .replace('\\', '').replace('&quot;', '"')
+            .re('"hls":"{"src":"(https?.*?\.m3u8)"')
+            )
+
+    dash = (D().css('#video').attr('data-parameters')
+           .replace('\\', '').replace('&quot;', '"')
+           .re('"dash":"{"src":"(https?.*?\.(?:mpd|m3u8))"')
+           )
+
+
---
+++ source_v2/player/kodik.py
@@ -0,0 +1,104 @@
+from ssc_codegen import DictSchema, ItemSchema, ListSchema, D, N, R
+
+
+class MovieTranslationsPanel(ListSchema):
+    """Representation available dubbers and params. Useful for switch dubber"""
+    # element signature eg:
+    # < option
+    # value = "1291"
+    # data - id = "1291"
+    # data - translation - type = "subtitles"
+    # data - media - id = "1132855"
+    # data - media - hash = "00c597737850b65972d22c112ddec73a"
+    # data - media - type = "seria"
+    # data - title = "Crunchyroll.Subtitles"
+    # > Crunchyroll.Subtitles < / option >
+
+    __SPLIT_DOC__ = D().css('.movie-translations-box').css_all('option')
+
+    name = D().text().trim(" ")
+    value = D().attr('value')
+    data_id = D().attr('data-id')
+    data_translation_type = D().attr('data-translation-type')
+    data_media_hash = D().attr('data-media-hash')
+    data_media_type = D().attr('data-media-type')
+    data_title = D().attr('data-title')
+
+
+class KodikAPIPayload(ItemSchema):
+    # single params pairs
+    d = R().re('var\s*domain\s+=\s+[\'"](.*?)[\'"];')
+    d_sign = R().re('var\s*d_sign\s+=\s+[\'"](.*?)[\'"];')
+    pd = R().re('var\s*pd\s+=\s+[\'"](.*?)[\'"];')
+    pd_sign = R().re('var\s*pd_sign\s+=\s+[\'"](.*?)[\'"];')
+    ref = R().re('var\s*ref\s+=\s+[\'"](.*?)[\'"];')
+    ref_sign = R().re('var\s*ref_sign\s+=\s+[\'"](.*?)[\'"];')
+    type = R().re('videoInfo\.type\s*=\s*[\'"](.*?)[\'"];')
+    hash = R().re('videoInfo\.hash\s*=\s*[\'"](.*?)[\'"];')
+    id = R().re('videoInfo\.id\s*=\s*[\'"](.*?)[\'"];')
+
+
+class KodikPage(ItemSchema):
+    """this schema used to extract params for next API request
+
+    required next keys for API request:
+        - contain in `api_payload` key
+    constants:
+        - cdn_is_working: true
+        - bad_user: false (or true)
+        - info: {}
+
+    USAGE:
+        1. GET <PLAYER_LINK> (e.g.: https://kodik.info/seria/1133512/04d5f7824ba3563bd78e44a22451bb45/720p)
+        2. parse payload (see required pairs upper) (<PAYLOAD>)
+        3. extract the API path from player_js_path (<API_PATH>) (encoded in BASE64)
+        4. POST https://kodik.info/ + <API_PATH>; data=<PAYLOAD> (<JSON>). next HEADERS required:
+            - origin="https://<NETLOC>" // player page
+            - referer=<PLAYER_LINK> // FIRST URL player entrypoint
+            - accept= "application/json, text/javascript, */*; q=0.01"
+        5. extract data from ['links'] key from <JSON> response
+        6. urls encoded in ROT_13 + BASE64 ciphers
+    ISSUES:
+        - kodik maybe have another netloc (e.g.: anivod)
+        - 403 Forbidden if request sent not from CIS region
+        - 404 DELETED: eg: https://kodik.info/seria/310427/09985563d891b56b1e9b01142ae11872/720p
+
+    """
+    # original player decode signature:
+    # function (e) {
+    #   var t;
+    #   e.src = (
+    #     t = e.src,
+    #     atob(
+    #       t.replace(
+    #         /[a-zA-Z]/g,
+    #         function (e) {
+    #           return String.fromCharCode((e <= 'Z' ? 90 : 122) >= (e = e.charCodeAt(0) + 13) ? e : e - 26)
+    #         }
+    #       )
+    #     )
+    #   )
+    # }
+
+    # can be json unmarshal
+    # contains keys:
+    # ['d', 'd_sign', 'pd', 'pd_sign', 'ref', 'ref_sign', 'advert_debug', 'first_url']
+    url_params = R().re('var\s*urlParams\s*=\s*[\'"](\{.*\})[\'"]')
+    api_payload: KodikAPIPayload = N().sub_parser(KodikAPIPayload)
+
+    # kodik sometimes changes the API path. It must be extracted from the player source code
+    # (netloc excluded)
+    # api path - base64 encoded string
+    player_js_path = R().re('<script\s*type="text/javascript"\s*src="(/assets/js/app\.player_single.*?)">')
+
+    # TODO: maybe add extra information?
+    movie_translations: MovieTranslationsPanel = N().default(None).sub_parser(MovieTranslationsPanel)
+
+
+# TODO create function signature
+class KodikApiPath(ItemSchema):
+    """Extract the API path from js player source"""
+    # js api path signature examples:
+    # ... $.ajax({type:"POST",url:atob("L2Z0b3I="),cache:! ...
+    # ... $.ajax({type: 'POST', url:atob('L3RyaQ=='),cache: !1 ...
+    path = R().re('\$\.ajax\([^>]+,url:\s*atob\(["\']([\w=]+)["\']\)')
