9c31e6b99089a9686631c4e8eb164c509f33d539
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-06-11 22:21:27 +0300

    add jutsu

---
+++ source_v2/src/jutsu.py
@@ -0,0 +1,106 @@
+from typing import Optional, Sequence, List
+
+from ssc_codegen.schema import DictSchema, ItemSchema, ListSchema
+from ssc_codegen.document import D, N
+
+
+
+class OngoingPage(ListSchema):
+    """usage:
+
+    POST https://jut.su/anime/ongoing/
+    ajax_load=yes&start_from_page=1&show_search=&anime_of_user=
+
+    """
+
+    __SPLIT_DOC__ = D().css_all(".all_anime_global")
+
+    url = D().css("a").attr("href").format("https://jut.su{{}}")
+    title = D().css(".aaname").text()
+    # background: url('https://gen.jut.su/uploads/animethumbs/aaaa.jpg')  no-repeat;
+    thumbnail = D().css(".all_anime_image").attr("style").re("'(https?://.*?)'")
+    # <div class="aailines">
+    #         1094 серии
+    #         <br>
+    #         14 фильмов
+    # </div>
+    counts = D().css_all(".aailines").text().strip("\r\n").join(" ")
+
+
+class SearchPage(OngoingPage):
+    """
+    POST https://jut.su/anime/
+    ajax_load=yes&start_from_page=1&show_search=<QUERY>&anime_of_user=
+
+    EXAMPLE:
+        POST https://jut.su/anime/
+        ajax_load=yes&start_from_page=1&show_search=LA&anime_of_user=
+    """
+    pass
+
+
+class EpisodesView(ListSchema):
+    __SPLIT_DOC__ = D().css_all(".video")
+
+    title = D().text().strip(" ")
+    url = D().attr("href").format("https://jut.su{{}}")
+
+
+class AnimePage(ItemSchema):
+    """
+    GET https://jut.su/<ANIME PATH>
+
+    EXAMPLE:
+        GET https://jut.su/kime-no-yaiba/
+    """
+    # test cases:
+    # Смотреть Клинок, рассекающий демонов все серии и сезоны
+    # Смотреть ТораДора все серии
+    title = D().css(".anime_padding_for_title").text().re("Смотреть (.*?) все")
+    description = D().css_all(".uv_rounded_bottom span").text().join(" ")
+    # background: url('https://gen.jut.su/uploads/animethumbs/anime_toradora.jpg') no-repeat; background-size: 104px auto;
+    thumbnail = D().css(".all_anime_title").attr("style").re("'(https?://.*?)'")
+
+    episodes: EpisodesView = N().sub_parser(EpisodesView)
+
+
+class SourceView(DictSchema):
+
+    __SPLIT_DOC__ = D().css_all("#my-player > source")
+    __SIGNATURE__ = {"QUALITY": "URL"}
+    __KEY__ = D().default('null').attr('res')
+    __VALUE__ = D().default(None).attr('src')
+
+
+class SourcePage(ItemSchema):
+    """
+    GET https://jut.su/<ANIME PATH>/<SEASON?>/episode-<NUM>.html
+
+    NOTE: VIDEO PLAY REQUEST SHOULD HAVE THE SAME USER-AGENT AS AN API CLIENT
+
+    eg:
+
+    cl = Client(headers={"user-agent": "X"})
+
+    ...
+
+    s = SourcePage(doc).parse()
+
+    mpv s["url_1080"] # 403, FORBIDDEN
+
+    mpv s["url_1080"] --user-agent="Y" # 403, FORBIDDEN
+
+    mpv s["url_1080"] --user-agent="X" # 200, OK
+
+    EXAMPLE:
+        GET https://jut.su/kime-no-yaiba/season-1/episode-1.html
+
+    ISSUES:
+        CHECK 'null' KEY in 'video'. if it contains - videos not available
+
+        check block reasons regex patterns:
+
+        - 'block_video_text_str_everywhere\\\+' - К сожалению, это видео недоступно.
+        - 'block_video_text_str\\\+' - К сожалению, в России это видео недоступно.
+    """
+    videos: SourceView = N().sub_parser(SourceView)
