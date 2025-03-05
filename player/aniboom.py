cb4075974acb48e0db98e362840424e9a33077d9
georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-03-06 00:33:56 +0300

    data_parameters: data_parameters: add json serialization

--- player/aniboom.py
+++ player/aniboom.py
@@ -1,10 +1,38 @@
-from ssc_codegen import D, DictSchema, ItemSchema, ListSchema, N, R
+from ssc_codegen import D, DictSchema, ItemSchema, ListSchema, N, R, Json
 
+class Dash(Json):
+    src: str
+    type: str
+
+class Hls(Json):
+    src: str
+    type: str
+
+class DataParameters(Json):
+    id: str
+    error: str
+    domain: str
+    cdn: str
+    counter: str
+    duration: str
+    poster: str
+    thumbnails: str
+    dash: Dash
+    hls: Hls
+    quality: bool
+    qualityVideo: int
+    vast: bool
+    country: str
+    platform: str
+    rating: str
+    nshowbl: bool
+    limitRate: bool
+    aBlocklimitRate: bool
 
 class AniboomPage(ItemSchema):
     """Extract MPD and M3U8 urls
 
-    Required `referer="https://animego.org/` HEADER
+    Required `referer="https://animego.org/` HEADER (.me, .club?)
 
     USAGE:
         1. GET <PLAYER_LINK> (e.g. https://aniboom.one/embed/6BmMbB7MxWO?episode=1&translation=30)
@@ -16,7 +44,7 @@ class AniboomPage(ItemSchema):
         - Origin="https://aniboom.one"
     ISSUES:
         - 403 Forbidden if request sent not from CIS region
-        - KEYS SHOULD BE STARTED IN Title case else hls/mpd links returns 403 error
+        - KEYS SHOULD BE STARTED IN Title Case else hls/mpd links returns 403 error
         - Sometimes, aniboom backend missing MPD key and returns M3U8 url. Check this value before usage:
 
         https://github.com/vypivshiy/ani-cli-ru/issues/29
@@ -44,33 +72,20 @@ class AniboomPage(ItemSchema):
 
 
     """
-
-    # can be json unmarshal
-    # eg signature:
-    # {"id":"WlmXYpNLMK0",
-    # "error":"/video-error/WlmXYpNLMK0",
-    # "domain":"animego.org",
-    # "cdn":"/cdn/WlmXYpNLMK0",
-    # "counter":"/counter/WlmXYpNLMK0",
-    # "duration":"1437",
-    # "poster":"https://i1.boom-img.com/wl/WlmXYpNLMK0/mqdefault.webp",
-    # "thumbnails":"https://i1.boom-img.com/wl/WlmXYpNLMK0/thumbnails/thumbnails.vtt",
-    #
-    # YES, THIS KEYS contains STRING
-    # "dash":"{
-    #   "src":"https://lily.yagami-light.com/wl/WlmXYpNLMK0/qqppjxsz6fvkf.mpd",
-    #   "type":"application/dash+xml"
-    #   }",
-    # "hls":"{
-    #   "src":"https://lily.yagami-light.com/wl/WlmXYpNLMK0/master_device.m3u8",
-    #   "type":"application/x-mpegURL"}",
-    #
-    # "quality":true, "qualityVideo":1080, "vast":true, "country":"RU",
-    # "platform":"Linux","rating":"16+","nshowbl":false,"limitRate":false,
-    # "aBlocklimitRate":false}
     data_parameters = (
-        D().css("#video").attr("data-parameters").repl("\\", "").repl("&quot;", '"')  # json unescape
-    )
+        D().css("#video").attr("data-parameters")
+        # unescape json
+        .repl("\\", "")
+        .repl('&quot;}', '}')
+        .repl('&quot;{', '{')
+        .repl("&quot;", '"')
+        # if backend parser try automatically unquote raw json
+        # fix it
+        .repl('}"', '}')
+        .repl('"{', '{')
+    ).jsonify(DataParameters)
+
+    # backport for old API impl realizations
     hls = (
         D()
         .css("#video")
